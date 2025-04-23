bfa6d7b5fb5696c8a0c0351eb97cbb3864bcb6f9
Наталья Калинина <115949717+bukonatali@users.noreply.github.com>
2025-04-23 18:40:54 +0300

    Add files via upload

---
+++ MyArrayDataException.java
@@ -0,0 +1,8 @@
+package org.example.Lesson_12;
+
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
