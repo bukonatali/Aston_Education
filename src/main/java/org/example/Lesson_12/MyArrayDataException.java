c2ccb953fc529d2c2f49b34100dce3df6bb98345
Наталья Калинина <115949717+bukonatali@users.noreply.github.com>
2025-04-27 21:06:46 +0300

    Delete src/main/java/org/example/Lesson_12 directory

--- src/main/java/org/example/Lesson_12/MyArrayDataException.java
+++
@@ -1,8 +0,0 @@
-package org.example.Lesson_12;
-
-class MyArrayDataException extends Exception {
-    public MyArrayDataException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
-
